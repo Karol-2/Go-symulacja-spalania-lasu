0631577c8218be2bbae9baccc40f3603a6e43421
Karol Krawczykiewicz <97386279+Karol-2@users.noreply.github.com>
2023-03-14 17:26:57 +0100

    lab04 zad1 i prawie 2

---
+++ lab04/zad1.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// na nastepnych zajęciach będzie jakby sprawdzian czego się nauczyliśmy
+	wektor1 := make([]int64, 20)
+	wektor2 := make([]int64, 20)
+	zsumowane := make([]int64, 20)
+
+	for i := 0; i < 20; i++ {
+		wektor1[i] = 2
+		wektor2[i] = 3
+		zsumowane[i] = wektor1[i] + wektor2[i]
+	}
+
+	fmt.Println("wektor1: ", wektor1)
+	fmt.Println("wektor2: ", wektor2)
+	fmt.Println("suma wektorów", zsumowane)
+
+}
