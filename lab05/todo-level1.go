f719683050dbaff8ec1504a0f085475df1e7a26b
Karol Krawczykiewicz <97386279+Karol-2@users.noreply.github.com>
2023-04-04 10:55:17 +0200

    lab05 - zadania 1 i 2

---
+++ lab05/todo-level1.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	file, err := os.Open("./lab05/todo.txt")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error:", err)
+	}
+
+}
---
+++ lab05/todo-level2.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	file, err := os.Open("./lab05/todo.txt")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if line[0:1] == "#" {
+			continue
+		}
+
+		switch line[0:3] {
+		case "[x]":
+			fmt.Println("\033[32m", line, "\033[0m") // zielony
+		case "[ ]":
+			fmt.Println("\033[31m", line, "\033[0m") // czerwony
+		case "[-]":
+			fmt.Println("\033[90m", line, "\033[0m") // szary
+		case "[+]":
+			fmt.Println("\033[33m", line, "\033[0m") // żółty
+		default:
+			fmt.Println(line)
+		}
+
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error:", err)
+	}
+
+}
